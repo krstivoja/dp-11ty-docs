9b2a4bce1a7ad5a4de4dd377ce439947920b6fa5
Luca <7936889+changethe@users.noreply.github.com>
2023-03-08 13:13:22 +0100

    cleaner

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,17 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+
+    /********************************** custom utilities **********************************/
+    require('tailwindcss/plugin')(function ({ addUtilities }) {
+      addUtilities({
+        // container
+        '.container-narrow': {
+          '@apply mx-auto w-[90%] max-w-[1200px]': {},
+        },
+      })
+    }),
+  ],
 }
